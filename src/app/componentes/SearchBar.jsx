2b810f3e2f7f3f014edc5195dcf1a49ad3de68d7
DESKTOP-K5J93E7\mabby <bernardoa.marquezg@gmail.com>
2023-03-24 11:36:50 -0600

    Avance: btn en search bar muestra/esconde newpost

--- src/app/componentes/SearchBar.jsx
+++ src/app/componentes/SearchBar.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { css } from "@emotion/react";
+import { useState, useRef } from "react";
 
 // css global
 import { SearchBarStyle } from "../Ye"
 import { NewPost } from "./NewPost";
 
 export function SearchBar() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const ref_btn = useRef(null)
+
+    const toggleSidebar = () => {
+      setIsSidebarOpen(!isSidebarOpen);
+    }
+
     return (
         <SearchBarStyle className="SearchBar">
             <input className="search" type="text" />
-            <input className="btn" type="button" value="+"/>
-            <NewPost className="form" />
+            <input className="btn" type="button" value={isSidebarOpen ? '^' : '+'} ref={ref_btn} onClick={()=>toggleSidebar()}/>
+            {isSidebarOpen && <NewPost className="form" />}
+            
         </SearchBarStyle>
-    );
+    )
 }
