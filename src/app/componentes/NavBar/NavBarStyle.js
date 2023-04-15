87cbd7c90a617cc8914bf0ace7736b772c43e4f0
DESKTOP-K5J93E7\mabby <bernardoa.marquezg@gmail.com>
2023-04-15 14:52:27 -0600

    Ajuste: fondo NavBar

--- src/app/componentes/NavBar/NavBarStyle.js
+++ src/app/componentes/NavBar/NavBarStyle.js
@@ -3,7 +3,6 @@ import styled from '@emotion/styled';
 export const NavBarStyle = styled.nav`
   grid-area: 1 / 1 / 2 / 2;
 
-  background-color: var(--fondo_seccion);
   a {
     text-decoration: none;
     color: var(--texto_principal);
