27740def6c1356384cbc9f5555f0f40faea952b5
DESKTOP-K5J93E7\mabby <bernardoa.marquezg@gmail.com>
2023-04-22 17:35:18 -0600

    Merge branch 'C-Cambios' of https://github.com/mabby1110/Artado into C-Cambios

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/app'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJTkbS0UtlAIWPihPCJDfPDtltCK-A7zg",
+  authDomain: "artado-77079.firebaseapp.com",
+  projectId: "artado-77079",
+  storageBucket: "artado-77079.appspot.com",
+  messagingSenderId: "874320070624",
+  appId: "1:874320070624:web:f6cbab86d29c25e1c7cd85"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
